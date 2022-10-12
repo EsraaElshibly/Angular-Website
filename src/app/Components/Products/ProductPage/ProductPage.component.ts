dac30921b9732d34bda9322038ee8ef84b963f20
EsraaElshibly <esraa.alshbly@gmail.com>
2022-10-13 01:11:36 +0200

    Change total order price according to products in orderList

--- src/app/Components/Products/ProductPage/ProductPage.component.ts
+++ src/app/Components/Products/ProductPage/ProductPage.component.ts
@@ -59,5 +59,20 @@
       }
     }
 
+    // Calculate Total price for all order by using accessor decorator (get)
+    get totalPrice () 
+    {
+      return this.orderList?.reduce(
+        (sum, product) => ({
+          AvailablePieces: 1,
+          // calcuate the below evealluation to get total price
+          ProductPrice: sum.ProductPrice + product.AvailablePieces * product.ProductPrice
+        }),
+
+        // Set Intial value to make price = 0$ if there is no order
+        {AvailablePieces:1, ProductPrice: 0}
+      ).ProductPrice
+    }
+
   }
 
